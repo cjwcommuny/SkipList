78f844467fc243cd1f5b2a78b51d35d4e47ca99d
cjwcommuny <cjwcommuny@outlook.com>
2018-06-05 10:41:13 +0800

    tell tester

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 /* C++ 11 required */
+/* Coding = UTF-8 */
 #include <iostream>
 #include "skiplist.h"
 /*
  * skiplist的节点是node类。skiplist作为一个driver，其中存储包含了首个dummy node和尾部的dummy node。
  * 这两个dummy node中的key分别存储了INT_MIN与INT_MAX，作为sentinal
  * node类中用vector容器存储该节点各层的指向下一个节点的指针
- * 对于tester来说，需要调用skiplist的类
+ * 对于tester来说，需要调用skiplist的类，来测试方法：del, insert, find, 构造函数；可以使用print方法检查方法调用后的正确性
+ * 并且tester还要测试时间性能
  * */
 
 
